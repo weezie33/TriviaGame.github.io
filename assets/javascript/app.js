673316598776b3ea43a65c4d28e8477b3e230185
Wezzie33 <corey.webeck@gmail.com>
2019-03-21 23:49:32 -0400

    TriviaGame.github.io

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,12 @@
-window.onload = function() {
+//load on browser refresh
+window.onload = function () {
   let intervalId;
-  let timer = 90;
+  let timer = 45;
   var correct = 0;
   var incorrect = 0;
-  //need set of questions
+  var unanswered = 0;
 
-
+//Questions
 let questionArray = [{
     question: " Which number is the largest integer?",
     answers: [" 65", " -65", " 65.5", " -65.5"],
@@ -43,28 +44,22 @@ let questionArray = [{
     nextQuestion();
   }
 
+//Retrieves questions
   function getQuestion() {
-    //Get first question
+
 
     for (i = 0; i < questionArray.length; i++) {
       $("#quiz-area").append("<h2>" + questionArray[i].question + "</h2>");
 
-      //Loop through question array and create buttons for each answer
-      // Clear button div of any newly created buttons
+//reviews questions array and generates selection
 
       for (let k = 0; k < questionArray[i].answers.length; k++) {
         $("#quiz-area").append(
-          "<h3><input type='radio' name='question-"  +
-            i +
-            "' value='" +
-            questionArray[i].answers[k] +
-            "''>" +
-            questionArray[i].answers[k]
-        );
-      }
-    }
-    $("#quiz-area").append("<button id='submit-btn'>Done</button>");
-  }
+          "<h3><input type='radio' name='question-"  + i + "' value='" + questionArray[i].answers[k]  + "''>" + questionArray[i].answers[k]);
+                }
+            }
+            $("#quiz-area").append("<button id='submit-btn'>Done</button>");
+        }
 
   function score() {
     //Queston 1
@@ -73,6 +68,7 @@ let questionArray = [{
         correct++;
       } else {
         incorrect++;
+
       }
     });
 
@@ -82,6 +78,7 @@ let questionArray = [{
         correct++;
       } else {
         incorrect++;
+
       }
     });
 
@@ -90,26 +87,7 @@ let questionArray = [{
       if ($(this).val() === questionArray[2].correctAnswer) {
         correct++;
       } else {
-        incorrect++;
-      }
-    });
-
-    //Question 4
-    $.each($("input[name='question-3']:checked"), function() {
-      if ($(this).val() === questionArray[3].correctAnswer) {
-        correct++;
-      } else {
-        incorrect++;
-      }
-    
-    });
-
-    //Question 5
-    $.each($("input[name='question-4']:checked"), function() {
-      if ($(this).val() === questionArray[4].correctAnswer) {
-        correct++;
-      } else {
-        incorrect++;
+        incorrect++; 
       }
     });
   }
@@ -117,30 +95,37 @@ let questionArray = [{
 function results(){
   $("#quiz-area").empty();
   $("#timer-area").empty();
-  $("#quiz-area").append("<h3>Correct:  " + correct + "</h3>");
-  $("#quiz-area").append("<h3> Incorrect:  " + incorrect + "</h3>");
+  $(".row").append("<h5>Correct:  " + correct + "</h5>");
+  $(".row").append("<h5>Incorrect:  " + incorrect + "</h5>");
+  $(".row").append("<h5>Unanswered:  " + unanswered + "</h5>");
+
 
   if(correct > incorrect) {
-    $("#quiz-area").prepend("<h4>Winner!</h4><br>")
+    $(".row").prepend("<h4>Winner!</h4><br>")
   } else {
-    $("#quiz-area").prepend("<h4>You Suck!</h4><br>")
-  }
+      $(".row").prepend("<h4>You Suck!</h4><br>")
+  };
 }
-  
+
   // Click Events
   $(document).on("click", "#startGame", function() {
+    
+    $("#timer-area").show();
     $("#startGame").replaceWith();
 
     startTimer();
     decrement();
     getQuestion();
+
   });
 
   $(document).on("click", "#submit-btn", function() {
     score();
     results();
   });
+
+function timeUp() {
+setTimeout(timesUp, 1000 * 45);
+    $(document.container).append("Time's Up!");
+    }
 };
-
-
-
